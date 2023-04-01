aca56f5c12bb44512b9b4d622dbb61a6035e68e6
Aleksandr Razumov <ernado@ya.ru>
2023-04-01 15:01:53 +0300

    ci: try

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export GOARCH=riscv64
+export GOOS=linux
+
+set -e
+set -o pipefail
+
+if [[ $# -eq 0 ]] ; then
+    echo 'No argument provided'
+    exit 1
+fi
+
+VERSION=$1
+
+echo "Building version ${VERSION}"
+
+
+OUTPUT="./out/${VERSION}/"
+
+set -x
+mkdir -p "${OUTPUT}"
+pushd "${OUTPUT}"
+
+curl --proto '=https' --tlsv1.2 -sSf "https://dl.google.com/go/go${VERSION}.src.tar.gz" | tar -xz
+
+pushd go/src
+./bootstrap.bash
+./make.bash
