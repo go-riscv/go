09e3376987a26ca90a7b2d53e9cd69170a52e304
Aleksandr Razumov <ernado@ya.ru>
2023-04-01 15:28:56 +0300

    ci: boostrap

--- build.sh
+++ build.sh
@@ -2,19 +2,26 @@
 
 export GOARCH=riscv64
 export GOOS=linux
+GOROOT_BOOTSTRAP="$(realpath ./out/go-linux-riscv64-bootstrap)"
 
 set -e
 set -o pipefail
 
 if [[ $# -eq 0 ]] ; then
-    echo 'No argument provided'
-    exit 1
+  echo 'No argument provided'
+  exit 1
+fi
+
+if [ ! -d "${GOROOT_BOOTSTRAP}" ]
+then
+  echo "No bootstrap found at ${GOROOT_BOOTSTRAP}}"
+  exit 1
+else
+  echo "Bootstrap: ${GOROOT_BOOTSTRAP}"
 fi
 
 VERSION=$1
-
 echo "Building version ${VERSION}"
-
 
 OUTPUT="./out/${VERSION}/"
 
