d9b05a4cfe18357dd7a87ca66075f549724cefd7
Aleksandr Razumov <ernado@ya.ru>
2023-04-01 16:46:41 +0300

    ci: pack

---
+++ pack.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ $# -eq 0 ]] ; then
+  echo 'No argument provided'
+  exit 1
+fi
+
+
+VERSION=$1
+
+pushd "./out/${VERSION}/go/bin/"
+  bindir=linux_riscv64
+  if [ -d "${bindir}" ]
+  then
+    echo "Moving binaries from ${bindir}"
+    mv ${bindir}/* .
+    rm -rf ${bindir}
+  fi
+popd
+
+pushd ./out
+  archive="go${VERSION}.linux-riscv64.tar.gz"
+  rm -f "${archive}"
+  echo "Creating ${archive}"
+  tar -C "${VERSION}" -czf "${archive}" go
+  sha256sum "${archive}" > "${archive}.sha256.txt"
+  cat "${archive}.sha256.txt"
+  ls --human-readable --kibibytes -Sl "${archive}"
+popd
