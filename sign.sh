60c54805146bb4b136fda59a435dc70fd2e8e051
Aleksandr Razumov <ernado@ya.ru>
2023-04-02 15:21:17 +0300

    ci: add gpg signatures

---
+++ sign.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+ver="${GOVERSION:-1.20.2}"
+archive="go${ver}.linux-riscv64.tar.gz"
+bootstrap=go-linux-riscv64-bootstrap.tbz
+src=go${ver}.src.tar.gz
+sums=checksums.sha256.txt
+key=943040B9817AC4C7
+
+cd _out
+for file in "${archive}" "${bootstrap}" "${src}" "${sums}"; do
+  gpg --batch --yes --default-key "${key}" --output "${file}.asc" --armor --detach-sig "${file}"
+  gpg --batch --default-key "${key}" --verify "${file}.asc" "${file}"
+done
